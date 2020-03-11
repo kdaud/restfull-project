601323f9898909b6fb610d7b12ba493942f35b2b
kdaud <dkakumirizii@gmail.com>
2020-03-11 19:02:24 +0300

    mycodes

--- src/main/java/com/example/demo/ProgramResource.java
+++ src/main/java/com/example/demo/ProgramResource.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProgramResource {
 	
+	@RequestMapping("programmer")
 	public List<Programmer> getProgrammers() {
 		List<Programmer> programmer = new ArrayList<Programmer>();
 		Programmer a1 = new Programmer();
@@ -19,8 +21,14 @@ public class ProgramResource {
 		a2.setName("B. Fernandez");
 		a2.setCourse("BSN");
 		
+		Programmer a3 = new Programmer();
+		a3.setId(14);
+		a3.setName("Gama Richard");
+		a3.setCourse("BNF");
+		
 		programmer.add(a1);
 		programmer.add(a2);
+		programmer.add(a3);
 		return programmer;
 	}
 }
