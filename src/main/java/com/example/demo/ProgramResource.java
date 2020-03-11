9d294716616c6ee2c3f9e802048029379746c110
kdaud <dkakumirizii@gmail.com>
2020-03-11 17:02:24 +0300

    Coding

--- src/main/java/com/example/demo/ProgramResource.java
+++ src/main/java/com/example/demo/ProgramResource.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProgramResource {
 	
-	public List<Programmer> getProgrammer() {
-		return new Programmer();
+	public List<Programmer> getProgrammers() {
+		List<Programmer> programmer = new ArrayList<Programmer>();
+		Programmer a1 = new Programmer();
+		a1.setId(10);
+		a1.setName("Daud Kakumirizi");
+		a1.setCourse("Software Engineering");
+		Programmer a2 = new Programmer();
+		a2.setId(11);
+		a2.setName("B. Fernandez");
+		a2.setCourse("BSN");
+		
+		programmer.add(a1);
+		programmer.add(a2);
+		return programmer;
 	}
 }
--- src/main/java/com/example/demo/Programmer.java
+++ src/main/java/com/example/demo/Programmer.java
@@ -3,4 +3,52 @@ package com.example.demo;
 
 public class Programmer {
 	
+	private int id;
+	
+	private String name;
+	
+	private String course;
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the course
+	 */
+	public String getCourse() {
+		return course;
+	}
+	
+	/**
+	 * @param course the course to set
+	 */
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
 }
